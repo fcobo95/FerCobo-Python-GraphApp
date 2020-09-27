62eea692209ad1179591c70c63151f086069c307
Fernando Cobo <fcobo95@gmail.com>
2020-09-27 14:07:54 -0600

    finalizing minor tweaks and fixing the broken app.cs reference

--- graph_tutorial/tutorial/apps.py
+++ graph_tutorial/tutorial/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class TutorialConfig(AppConfig):
     name = 'tutorial'
+    
+# This looks like the app itself, setting the name 'tutorial' to it, 
+# as used in the other classes and part of the application.
---
+++ markdown-docs/Application Status.md
@@ -0,0 +1,1 @@
+#Application Completion Status
